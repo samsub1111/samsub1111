72a103eaa353c5a3e1129c3ed8b90631701e3d63
“samsub1111” <premshiva2016@gmail.com>
2024-05-20 14:41:51 -0400

    First project initialization

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>CSS Margin and Padding Mastery</h1>
+      </header>
+      <main>
+        <div className="box box1">Box 1</div>
+        <p className="paragraph"> //This is a paragraph to show Padding</p>
+      </main>
+      <footer>
+        <p>This is footer</p>
+      </footer>
+    </div>
+  );
+}
+export default App;
